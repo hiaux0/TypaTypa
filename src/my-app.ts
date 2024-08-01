082e30d459879672da514062492edb7c330d4912
Your Name <you@exmple.com>
2024-08-01 14:26:37 +0200

    feat(typing): init working minimal typing

--- src/my-app.ts
+++ src/my-app.ts
@@ -1,13 +1,41 @@
 export class MyApp {
   public message = "Typing App cua Emy";
-  public typedText = "Hello ";
-  public currentLetter = "w";
-  public currentTextToType = "we are typing";
+  //public typedText = "Hello ";
+  //public currentLetter = "w";
+  //public currentTextToType = "e are typing";
+  public typedText = "";
+  public currentLetter = "";
+  public currentTextToType = "hello we are typing";
 
   attached() {
+    this.initTyping();
+
     document.addEventListener("keydown", (event) => {
-      const key = event.key
+      const key = event.key;
       /*prettier-ignore*/ console.log("[my-app.ts,8] key: ", key);
+      if (key === this.currentLetter) {
+        this.typedText += key;
+        this.currentLetter = this.currentTextToType[0];
+        this.currentTextToType = this.currentTextToType.slice(1);
+      }
+
+      if (this.currentTextToType === "") {
+        this.resetTyping();
+        this.initTyping();
+      }
     });
+  }
+
+  private initTyping() {
+    if (this.currentLetter === "") {
+      this.currentLetter = this.currentTextToType[0];
+      this.currentTextToType = this.currentTextToType.slice(1);
+    }
+  }
+
+  private resetTyping() {
+    this.typedText = "";
+    this.currentLetter = "";
+    this.currentTextToType = "hello we are typing";
   }
 }
