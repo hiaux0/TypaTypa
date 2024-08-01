5db21432a78484d98db0a4645e684c243cdb41b6
Your Name <you@exmple.com>
2024-08-01 14:15:48 +0200

    feat(typing): start

--- src/my-app.ts
+++ src/my-app.ts
@@ -1,3 +1,13 @@
 export class MyApp {
-  public message = 'Typing App cua Emy';
+  public message = "Typing App cua Emy";
+  public typedText = "Hello ";
+  public currentLetter = "w";
+  public currentTextToType = "we are typing";
+
+  attached() {
+    document.addEventListener("keydown", (event) => {
+      const key = event.key
+      /*prettier-ignore*/ console.log("[my-app.ts,8] key: ", key);
+    });
+  }
 }
