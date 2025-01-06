e977ab0723a6c4bca2e50957efb4eba3d4389217
Your Name <you@exmple.com>
2025-01-07 01:28:17 +0700

    chore: fix vite warnings

--- src/ui/value-converters/load-component-value-converter.ts
+++ src/ui/value-converters/load-component-value-converter.ts
@@ -2,6 +2,7 @@ import { pascalCase } from "aurelia";
 
 export class LoadComponentValueConverter {
   async toView(componentPath: string) {
+    /* @vite-ignore */
     const object = await import(componentPath);
     const keys = Object.keys(object);
     const name = pascalCase(keys[0]);
--- src/ui/value-converters/load-template-value-converter.ts
+++ src/ui/value-converters/load-template-value-converter.ts
@@ -1,5 +1,6 @@
 export class LoadTemplateValueConverter {
   async toView(templatePath: string) {
+    /* @vite-ignore */
     const raw = await import(templatePath);
     return raw.template;
   }
