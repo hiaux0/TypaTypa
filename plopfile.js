84872c91274e0806d3812200ad914cb237b82dd4
Your Name <you@exmple.com>
2025-01-14 12:06:34 +0700

    chore(generator.plop): add ui component templates

--- plopfile.js
+++ plopfile.js
@@ -1,8 +1,45 @@
-export default function (plop) {
-	// create your generators here
-	plop.setGenerator('basics', {
-		description: 'this is a skeleton plopfile',
-		prompts: [], // array of inquirer prompts
-		actions: []  // array of actions
-	});
-};
+export default function (
+  /** @type {import('plop').NodePlopAPI} */
+  plop
+) {
+  // controller generator
+  plop.setGenerator("Ui Component", {
+    description: "Create a new Ui Component",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "Ui Component name"
+      },
+      {
+        type: "list",
+        name: "type",
+        message: "Ui Component type",
+        choices: ["atoms", "molecules", "organisms", "pages"]
+      }
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "src/ui/{{type}}/ui-{{name}}/ui-{{name}}.ts",
+        templateFile: "src/ui/templates/ui-template-hbs/ui-template.ts.hbs"
+      },
+      {
+        type: "add",
+        path: "src/ui/{{type}}/ui-{{name}}/ui-{{name}}.html",
+        templateFile: "src/ui/templates/ui-template-hbs/ui-template.html.hbs"
+      },
+      {
+        type: "add",
+        path: "src/ui/{{type}}/ui-{{name}}/ui-{{name}}-demo/ui-{{name}}-demo.ts",
+        templateFile: "src/ui/templates/ui-template-hbs/ui-template.ts.hbs"
+      },
+      {
+        type: "add",
+        path: "src/ui/{{type}}/ui-{{name}}/ui-{{name}}-demo/ui-{{name}}-demo.html",
+        templateFile: "src/ui/templates/ui-template-hbs/ui-template.html.hbs"
+      }
+      // demo
+    ]
+  });
+}
