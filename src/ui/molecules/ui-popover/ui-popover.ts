d96fe88c560c16cd0ae5872637e63bfc8c475334
Your Name <you@exmple.com>
2025-01-15 11:15:40 +0700

    works

--- src/ui/molecules/ui-popover/ui-popover.ts
+++ src/ui/molecules/ui-popover/ui-popover.ts
@@ -7,25 +7,42 @@ const adjust = 5;
 const popoverVariants = cva();
 
 const template = `
-<div ref="host" class="relative inline-block text-left" data-position.bind="position">
-  <button ref="trigger" class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+<div-ui-popover
+  ref="uiPopoverRef"
+  class="relative inline-block text-left"
+  data-position.bind="position"
+>
+  <button
+    if.bind="!host"
+    ref="trigger"
+    class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+  >
     <au-slot name="trigger"></au-slot>
   </button>
-  <div ref="content" class="hidden absolute mt-2 w-28 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" style.bind="contentPosition">
+  <div
+    ref="content"
+    class="
+      \${isOpen ? 'hidden' : ''}
+      absolute w-28 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none
+    "
+    style.bind="contentPosition"
+  >
     <au-slot name="content"></au-slot>
   </div>
-</div>
+</div-ui-popover
 `;
 
 @customElement({
   name: "ui-popover",
   template,
+  containerless: true,
 })
 export class UiPopover {
   @bindable() host: HTMLElement;
   @bindable() isOpen: boolean = true;
   @bindable() position: string = "bottom-right";
 
+  public uiPopoverRef: HTMLElement;
   public trigger: HTMLElement;
   public content: HTMLElement;
   public width: number;
@@ -42,7 +59,7 @@ export class UiPopover {
       case "bottom-left":
         return `top: 100%; left: 0; margin-top: 0.5rem;`;
       case "bottom-right":
-        return `top: 100%; left: ${this.width + adjust}px; margin-top: 0.5rem;`;
+        return `top: 100%; left: ${this.width + adjust}px;`;
       case "bottom":
         return `top: 100%; left: 50%; transform: translateX(-50%); margin-top: 0.5rem;`;
       default:
@@ -51,11 +68,14 @@ export class UiPopover {
   }
 
   attached() {
-    this.content.classList.toggle("hidden", !this.isOpen);
-    this.width = this.host.offsetWidth;
-    this.height = this.host.offsetHeight;
+    const finalElement = this.host ?? this.uiPopoverRef;
+    /*prettier-ignore*/ console.log("[ui-popover.ts,70] this.host: ", this.host);
+    this.width = finalElement.offsetWidth;
+    /*prettier-ignore*/ console.log("[ui-popover.ts,73] this.width: ", this.width);
+    this.height = finalElement.offsetHeight;
+    /*prettier-ignore*/ console.log("[ui-popover.ts,75] this.height: ", this.height);
 
-    this.trigger.addEventListener("click", () => {
+    this.trigger?.addEventListener("click", () => {
       this.toggle();
     });
 
@@ -66,11 +86,11 @@ export class UiPopover {
 
   private close = (): void => {
     this.isOpen = false;
-    this.content?.classList.toggle("hidden", !this.isOpen);
+    // this.content?.classList.toggle("hidden", !this.isOpen);
   };
 
   private toggle = (): void => {
     this.isOpen = !this.isOpen;
-    this.content?.classList.toggle("hidden", !this.isOpen);
+    // this.content?.classList.toggle("hidden", !this.isOpen);
   };
 }
